665fb80e1d5e3e4d86e70d9c60892655bafab34e
1000ch <shogo.sensui@gmail.com>
2016-05-01 18:13:00 +0900

    Put div instead of svg to use pseudo elements

--- app.js
+++ app.js
@@ -71,93 +71,90 @@ const colors = [
   { x : 8, y : 5, color : '#109121' },
   { x : 8, y : 6, color : '#0a5c15' },
 
-  { x : 9, y : 0, color : '#d1fad7' },
-  { x : 9, y : 1, color : '#9cf4a7' },
-  { x : 9, y : 2, color : '#66ee78' },
-  { x : 9, y : 3, color : '#30e849' },
-  { x : 9, y : 4, color : '#16c72e' },
-  { x : 9, y : 5, color : '#109121' },
-  { x : 9, y : 6, color : '#0a5c15' },
+  { x : 9, y : 0, color : '#ebfad1' },
+  { x : 9, y : 1, color : '#d4f49c' },
+  { x : 9, y : 2, color : '#bcee66' },
+  { x : 9, y : 3, color : '#a5e830' },
+  { x : 9, y : 4, color : '#86c716' },
+  { x : 9, y : 5, color : '#629110' },
+  { x : 9, y : 6, color : '#3e5c0a' },
 
-  { x : 10, y : 0, color : '#ebfad1' },
-  { x : 10, y : 1, color : '#d4f49c' },
-  { x : 10, y : 2, color : '#bcee66' },
-  { x : 10, y : 3, color : '#a5e830' },
-  { x : 10, y : 4, color : '#86c716' },
-  { x : 10, y : 5, color : '#629110' },
-  { x : 10, y : 6, color : '#3e5c0a' },
+  { x : 10, y : 0, color : '#faf9d1' },
+  { x : 10, y : 1, color : '#f4f19c' },
+  { x : 10, y : 2, color : '#eee966' },
+  { x : 10, y : 3, color : '#e8e230' },
+  { x : 10, y : 4, color : '#c7c116' },
+  { x : 10, y : 5, color : '#918d10' },
+  { x : 10, y : 6, color : '#5c590a' },
 
-  { x : 11, y : 0, color : '#faf9d1' },
-  { x : 11, y : 1, color : '#f4f19c' },
-  { x : 11, y : 2, color : '#eee966' },
-  { x : 11, y : 3, color : '#e8e230' },
-  { x : 11, y : 4, color : '#c7c116' },
-  { x : 11, y : 5, color : '#918d10' },
-  { x : 11, y : 6, color : '#5c590a' },
+  { x : 11, y : 0, color : '#faefd1' },
+  { x : 11, y : 1, color : '#f4dc9c' },
+  { x : 11, y : 2, color : '#eeca66' },
+  { x : 11, y : 3, color : '#e8b730' },
+  { x : 11, y : 4, color : '#c79816' },
+  { x : 11, y : 5, color : '#916f10' },
+  { x : 11, y : 6, color : '#5c460a' },
 
-  { x : 12, y : 0, color : '#faefd1' },
-  { x : 12, y : 1, color : '#f4dc9c' },
-  { x : 12, y : 2, color : '#eeca66' },
-  { x : 12, y : 3, color : '#e8b730' },
-  { x : 12, y : 4, color : '#c79816' },
-  { x : 12, y : 5, color : '#916f10' },
-  { x : 12, y : 6, color : '#5c460a' },
+  { x : 12, y : 0, color : '#fae6d1' },
+  { x : 12, y : 1, color : '#f4c89c' },
+  { x : 12, y : 2, color : '#eeaa66' },
+  { x : 12, y : 3, color : '#e88c30' },
+  { x : 12, y : 4, color : '#c76f16' },
+  { x : 12, y : 5, color : '#915110' },
+  { x : 12, y : 6, color : '#5c330a' },
 
-  { x : 13, y : 0, color : '#fae6d1' },
-  { x : 13, y : 1, color : '#f4c89c' },
-  { x : 13, y : 2, color : '#eeaa66' },
-  { x : 13, y : 3, color : '#e88c30' },
-  { x : 13, y : 4, color : '#c76f16' },
-  { x : 13, y : 5, color : '#915110' },
-  { x : 13, y : 6, color : '#5c330a' },
+  { x : 13, y : 0, color : '#f2e2d9' },
+  { x : 13, y : 1, color : '#e3c0ac' },
+  { x : 13, y : 2, color : '#d59f80' },
+  { x : 13, y : 3, color : '#c67d53' },
+  { x : 13, y : 4, color : '#a66037' },
+  { x : 13, y : 5, color : '#794628' },
+  { x : 13, y : 6, color : '#4d2c19' },
 
-  { x : 14, y : 0, color : '#f2e2d9' },
-  { x : 14, y : 1, color : '#e3c0ac' },
-  { x : 14, y : 2, color : '#d59f80' },
-  { x : 14, y : 3, color : '#c67d53' },
-  { x : 14, y : 4, color : '#a66037' },
-  { x : 14, y : 5, color : '#794628' },
-  { x : 14, y : 6, color : '#4d2c19' },
-
-  { x : 15, y : 0, color : '#ffffff' },
-  { x : 15, y : 1, color : '#e4e4e4' },
-  { x : 15, y : 2, color : '#b4b4b4' },
-  { x : 15, y : 3, color : '#848484' },
-  { x : 15, y : 4, color : '#545454' },
-  { x : 15, y : 5, color : '#242424' },
-  { x : 15, y : 6, color : '#000000' }
+  { x : 14, y : 0, color : '#ffffff' },
+  { x : 14, y : 1, color : '#e4e4e4' },
+  { x : 14, y : 2, color : '#b4b4b4' },
+  { x : 14, y : 3, color : '#848484' },
+  { x : 14, y : 4, color : '#545454' },
+  { x : 14, y : 5, color : '#242424' },
+  { x : 14, y : 6, color : '#000000' }
 ];
 
-const svg = d3.select('body').append('svg');
-const width = 720;
-const height = 440;
+const brightness = colors
+  .map(color => color.x)
+  .filter((color, i, array) => array.indexOf(color) === i);
 
-svg.attr('width', width)
-   .attr('height', height)
-   .attr('class', 'Palette');
+const grid = d3
+  .select('body')
+  .append('div')
+  .attr('class', 'Grid');
 
-const xMax = d3.max(colors, d => d.x) + 1;
-const yMax = d3.max(colors, d => d.y) + 1;
-const cellSize = 48;
-
-const color = svg.append('g')
-  .attr('transform', `translate(${width / 2}, ${height / 2})`)
-  .selectAll('.Item')
-  .data(colors)
+const columns = grid
+  .selectAll('.Grid')
+  .data(brightness)
   .enter()
-  .append('g')
-  .attr('class', 'Item')
-  .attr('transform', d => `translate(${(d.x - xMax / 2) * cellSize}, ${(d.y - yMax / 2) * cellSize})`);
+  .append('div')
+  .attr('class', 'Column');
 
-color.append('rect')
-  .attr('x', -cellSize / 2)
-  .attr('y', -cellSize / 2)
-  .attr('width', cellSize - 1)
-  .attr('height', cellSize - 1)
-  .attr('fill', d => d.color)
-  .attr('class', 'Color')
-  .attr('aria-label', d => d.color)
-  .attr('data-clipboard-action', 'copy')
-  .attr('data-clipboard-text', d => d.color);
+columns.each(function(column) {
+  d3.select(this)
+    .selectAll('.Cell')
+    .data(colors.filter(color => color.x === column))
+    .enter()
+    .append('div')
+    .attr('class', 'Cell')
+    .attr('aria-label', d => d.color)
+    .attr('data-oncopy-label', 'Copied!')
+    .attr('data-clipboard-action', 'copy')
+    .attr('data-clipboard-text', d => d.color)
+    .style('background-color', d => d.color)
+    .on('mouseout', function() {
+      this.classList.remove('Cell--copied');
+    });
+});
 
-const clipboard = new Clipboard('.Color');
+const clipboard = new Clipboard('.Cell');
+
+clipboard.on('success', e => {
+  e.trigger.classList.add('Cell--copied');
+});
